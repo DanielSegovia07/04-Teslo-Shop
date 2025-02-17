9745e46299065b45fad28641ff9a04511d5b2b90
DanielSegovia07 <danielsegovia671@gmail.com>
2025-02-17 16:27:56 -0600

    inicio seccion 15

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,6 +1,8 @@
 import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
 import { Socket } from 'socket.io';
+import { Delete } from '@nestjs/common';
+import { first } from 'rxjs';
 
 @WebSocketGateway({cors: true})
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -8,9 +10,15 @@ export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnec
     
   }
   handleConnection(client: Socket) {
-    console.log('Cliente conectado', client.id)
+    // console.log('Cliente conectado', client.id)
+    this.messageWsService.registerClient(client)
+
+
+
   }
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado', client.id)
+    // console.log('Cliente desconectado', client.id)
+    this.messageWsService.removeClient(client.id)
+    console.log({conectados : this.messageWsService.getConnectedClients()})
   }
 }
